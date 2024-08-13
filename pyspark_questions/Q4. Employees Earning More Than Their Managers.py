d7ce6252fc4ec6dcc95c876149f3c7adbb75794a
abhishekkr6059 <abhishekkr6059>
2024-08-13 10:36:18 +0000

    Employees Earning More Than Their Managers

---
+++ pyspark_questions/Q4. Employees Earning More Than Their Managers.py
@@ -0,0 +1,38 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC - Write a Pyspark program to find Employees Earning More Than Their Managers
+
+# COMMAND ----------
+
+# Define the schema for the "employees"
+employees_schema = StructType([
+StructField("id", IntegerType(), True),
+StructField("name", StringType(), True),
+StructField("salary", IntegerType(), True),
+StructField("managerId", IntegerType(), True)
+])
+ 
+# Define data for the "employees"
+employees_data = [
+(1, 'Joe', 70000, 3),
+(2, 'Henry', 80000, 4),
+(3, 'Sam', 60000, None),
+(4, 'Max', 90000, None)
+]
+
+# COMMAND ----------
+
+employees_df = spark.createDataFrame(employees_data, schema=employees_schema)
+display(employees_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - Implement a PYSPARK program to find the employee who is earning more than their manager
+# MAGIC |name|
+# MAGIC |----|
+# MAGIC |Joe|
+
+# COMMAND ----------
+
+
---
+++ pyspark_solutions/S4. Employees Earning More Than Their Managers.py
@@ -0,0 +1,43 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC - Write a Pyspark program to find Employees Earning More Than Their Managers
+
+# COMMAND ----------
+
+# Define the schema for the "employees"
+employees_schema = StructType([
+StructField("id", IntegerType(), True),
+StructField("name", StringType(), True),
+StructField("salary", IntegerType(), True),
+StructField("managerId", IntegerType(), True)
+])
+ 
+# Define data for the "employees"
+employees_data = [
+(1, 'Joe', 70000, 3),
+(2, 'Henry', 80000, 4),
+(3, 'Sam', 60000, None),
+(4, 'Max', 90000, None)
+]
+
+# COMMAND ----------
+
+employees_df = spark.createDataFrame(employees_data, schema=employees_schema)
+display(employees_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - Implement a PYSPARK program to find the employee who is earning more than their manager
+# MAGIC |name|
+# MAGIC |----|
+# MAGIC |Joe|
+
+# COMMAND ----------
+
+res_df = (
+    employees_df.alias("emp_df1").join(employees_df.alias("emp_df2"), col("emp_df1.managerId") == col("emp_df2.id"))
+    .filter(col("emp_df1.salary") > col("emp_df2.salary"))
+    .select("emp_df1.name")
+)
+display(res_df)
